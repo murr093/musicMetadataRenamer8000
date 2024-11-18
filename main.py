cd0de7e6adeeb11188db2233507df4b6837013a6
muraine9 <sayafranlp@gmail.com>
2024-11-18 19:40:02 +0300

    uhh, update before goi8ng to nix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from textual.app import App, ComposeResult
 from textual.widgets import Header, Footer, Button, Static
-from textual.containers import Horizontal, Vertical
+from textual.containers import Horizontal, Vertical, VerticalScroll
 from textual import events
 import os
 
@@ -8,19 +8,25 @@ class FileImportApp(App):
 
     CSS_PATH = "style.tcss"
 
+
+    # Creating the composition  
     def compose(self) -> ComposeResult:
 
         with Vertical():
-            yield Static("line",classes="line")
-            with Horizontal():
-                yield Static("two",classes="basecont")
-                yield Static("three",classes="basecont")
+            yield Static("line",classes="line") # The string
+            with Horizontal(): 
+                # yield Static("two",classes="modulesContainer") # Modules container 
+                with VerticalScroll(id="modulesContainer"):
+                    yield Static("module1",classes="module")
+                    yield Static("module2",classes="module")
+                    yield Static("module3",classes="module")
+                yield Static("three",classes="basecont") # "Didn't decide yet" container
 
         yield Header()
         yield Footer()
                     
 
-
+# Running stuff
 if __name__ == "__main__":
     app = FileImportApp()
     app.run()
